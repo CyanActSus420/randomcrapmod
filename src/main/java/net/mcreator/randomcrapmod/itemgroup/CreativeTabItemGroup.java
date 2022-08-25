c75aa5c49e52bead3e83379941b61ff9f3778d25
CyanActSus <HP_admin@DESKTOP-ALB6JIE>
2022-08-25 13:33:11 +0200

    creative tab lmao

---
+++ src/main/java/net/mcreator/randomcrapmod/itemgroup/CreativeTabItemGroup.java
@@ -0,0 +1,36 @@
+
+package net.mcreator.randomcrapmod.itemgroup;
+
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+
+import net.mcreator.randomcrapmod.item.DynamiteItem;
+import net.mcreator.randomcrapmod.RandomCrapModModElements;
+
+@RandomCrapModModElements.ModElement.Tag
+public class CreativeTabItemGroup extends RandomCrapModModElements.ModElement {
+	public CreativeTabItemGroup(RandomCrapModModElements instance) {
+		super(instance, 3);
+	}
+
+	@Override
+	public void initElements() {
+		tab = new ItemGroup("tabcreative_tab") {
+			@OnlyIn(Dist.CLIENT)
+			@Override
+			public ItemStack createIcon() {
+				return new ItemStack(DynamiteItem.block);
+			}
+
+			@OnlyIn(Dist.CLIENT)
+			public boolean hasSearchBar() {
+				return false;
+			}
+		};
+	}
+
+	public static ItemGroup tab;
+}
