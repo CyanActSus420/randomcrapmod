eccee2643f6b7de56e17f60760b45df2f3ead8ac
CyanActSus <HP_admin@DESKTOP-ALB6JIE>
2022-08-25 19:53:35 +0200

    very op diamond tnt

---
+++ src/main/java/net/mcreator/randomcrapmod/block/DiamondTNTBlock.java
@@ -0,0 +1,80 @@
+
+package net.mcreator.randomcrapmod.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.randomcrapmod.procedures.DiamondTNTRedstoneOnProcedure;
+import net.mcreator.randomcrapmod.itemgroup.CreativeTabItemGroup;
+import net.mcreator.randomcrapmod.RandomCrapModModElements;
+
+import java.util.stream.Stream;
+import java.util.Map;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Collections;
+import java.util.AbstractMap;
+
+@RandomCrapModModElements.ModElement.Tag
+public class DiamondTNTBlock extends RandomCrapModModElements.ModElement {
+	@ObjectHolder("random_crap_mod:diamond_tnt")
+	public static final Block block = null;
+
+	public DiamondTNTBlock(RandomCrapModModElements instance) {
+		super(instance, 4);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(CreativeTabItemGroup.tab)).setRegistryName(block.getRegistryName()));
+	}
+
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ORGANIC).sound(SoundType.WET_GRASS).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0));
+			setRegistryName("diamond_tnt");
+		}
+
+		@Override
+		public int getOpacity(BlockState state, IBlockReader worldIn, BlockPos pos) {
+			return 15;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+
+		@Override
+		public void neighborChanged(BlockState blockstate, World world, BlockPos pos, Block neighborBlock, BlockPos fromPos, boolean moving) {
+			super.neighborChanged(blockstate, world, pos, neighborBlock, fromPos, moving);
+			int x = pos.getX();
+			int y = pos.getY();
+			int z = pos.getZ();
+			if (world.getRedstonePowerFromNeighbors(new BlockPos(x, y, z)) > 0) {
+
+				DiamondTNTRedstoneOnProcedure.executeProcedure(Stream
+						.of(new AbstractMap.SimpleEntry<>("world", world), new AbstractMap.SimpleEntry<>("x", x),
+								new AbstractMap.SimpleEntry<>("y", y), new AbstractMap.SimpleEntry<>("z", z))
+						.collect(HashMap::new, (_m, _e) -> _m.put(_e.getKey(), _e.getValue()), Map::putAll));
+			} else {
+			}
+		}
+	}
+}
